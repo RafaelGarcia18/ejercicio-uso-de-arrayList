9ea6e52369bbc0dc794453d32488f6c5980cf70e
Rafael Garcia <regestra64@gmail.com>
2018-09-18 14:34:38 -0600

    guia arrayList metodo buscar arreglado

--- src/guias/de/Ayala/TratamientoDatos.java
+++ src/guias/de/Ayala/TratamientoDatos.java
@@ -5,25 +5,33 @@ import java.util.ArrayList;
 public class TratamientoDatos extends Utilidades {
 
     //metodo aun no terminado
-    public String buscar(ArrayList nombres,ArrayList apellidos) {
-        System.out.println("Que nombre o apellido desea encontrar??");
+    public String buscar(ArrayList list, int criterio) {
+        String mensaje =null;
+        switch(criterio){
+            case 1:
+                mensaje ="nombre";
+                break;
+            case 2:
+                mensaje = "apellido";
+                break;
+        }
+        System.out.println("Que "+mensaje+" desea encontrar??");
         String encontrar = entrada.nextLine();
         boolean find = false;
         int cuantosEncontrados = 0;
-        String encontradoN = "El nombre " + encontrar + " fue encontrado en la posicion : ";
-        String encontradoA = "El apellido " + encontrar + " fue encontrado en la posicion : ";
-        for (int i = 0; i < nombres.size(); i++) {
-            if (((String)nombres.get(i)).equals(encontrar)) {
+        String encontrado = "El "+mensaje+" '" + encontrar + "' fue encontrado en la posicion : ";
+        for (int i = 0; i < list.size(); i++) {
+            if (((String)list.get(i)).equals(encontrar)) {
                 if (cuantosEncontrados != 0) {
-                    encontradoN = encontradoN + ", ";
+                    encontrado = encontrado + ", ";
                 }
-                encontradoN = encontradoN + (i + 1);
+                encontrado = encontrado + (i + 1);
                 find = true;
                 cuantosEncontrados = cuantosEncontrados + 1;
             }
         }
         if (find) {
-            return encontradoN;
+            return encontrado;
         } else {
             return "Ningun resultado encontrado";
         }
